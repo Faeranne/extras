072df9fcb3f9ff7166be0dc9fbc53f0041889901
Blixa Morgan <blixa@projectmakeit.com>
2015-02-13 13:14:13 -0600

    move git files when installing

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 if [ $DIR != $HOME+'/.extras' ]; then
 	mkdir $HOME/.extras/
 	mv $DIR/* $(echo $HOME/.extras)
+	mv $DIR/.git* $(echo $HOME/.extras)
+	rm $DIR
 fi
 echo ". ~/.extras/bashinc" >> ~/.bashrc
